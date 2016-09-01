984f6ac066488dbc57aed5c337bfb02acfebb00a
OLDU <oldu@stageconcept.ch>
2016-09-01 14:56:42 +0200

    add some functionalities

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,11 +1,20 @@
+
+// inspired by: http://www.jcgonzalez.com/quick-mysql-commands-examples
+
 package com.company;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
+
+    private static final String TRUSTSTORE = "C:\\ssl\\test001\\truststore";    // Certificate Authority (CA)
+    private static final String TRUSTSTORE_PWD = "111111";
+    private static final String KEYSTORE = "C:\\ssl\\test001\\keystore";    // User certificate
+    private static final String KEYSTORE_PWD = "export";    // le pwd du fichier keystore, doit être le même que le pwd du fichier inclu dans le keystore.
 
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://192.168.1.114:3306"
@@ -18,19 +27,38 @@ public class Main {
 
     private static final long PAUSE_MILLIS = 1000;
 
-    public static void main(String[] args) {
+    private static void checkStoreFile(String path) {
+        System.out.println( "file: " + path);
 
-        System.setProperty("javax.net.ssl.keyStore","C:\\ssl\\test001\\keystore");
-        System.setProperty("javax.net.ssl.keyStorePassword","export");// le mdp du fichier keystore, doit être le même que le mdp du fichier inclu dans le keystore.
-        System.setProperty("javax.net.ssl.trustStore","C:\\ssl\\test001\\truststore");
-        System.setProperty("javax.net.ssl.trustStorePassword","111111"); // le mdp du fichier truststore.
-        System.setProperty("javax.net.debug","all");
+        File file = new File(path);
+
+        System.out.println( "exists: "  + file.exists() );
+        System.out.println( "is file: "  + file.isFile() );
+        System.out.println( "can read: " + file.canRead() );
+        System.out.println( "can write: " + file.canWrite() );
+        System.out.println( "is hidden: " + file.isHidden() );
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
 
         System.out.println();
         System.out.println("#####################");
         System.out.println();
-        System.out.println("keyStore: " + System.getProperty("javax.net.ssl.keyStore"));
-        System.out.println("truststore: " + System.getProperty("javax.net.ssl.trustStore"));
+
+        checkStoreFile(KEYSTORE);
+        checkStoreFile(TRUSTSTORE);
+
+        System.setProperty("javax.net.ssl.keyStore",KEYSTORE);
+        System.setProperty("javax.net.ssl.keyStorePassword",KEYSTORE_PWD);
+        System.setProperty("javax.net.ssl.trustStore",TRUSTSTORE);
+        System.setProperty("javax.net.ssl.trustStorePassword",TRUSTSTORE_PWD);
+        System.setProperty("javax.net.debug","all");    // network traffic to stdout
+
+        System.out.println("#####################");
+        System.out.println();
+        System.out.println("KeyStore System Property: " + System.getProperty("javax.net.ssl.keyStore"));
+        System.out.println("TrustStore System Property: " + System.getProperty("javax.net.ssl.trustStore"));
         System.out.println();
         System.out.println("#####################");
         System.out.println();
@@ -46,20 +74,11 @@ public class Main {
 
             ResultSet rs;
 
-            rs = conn.createStatement().executeQuery("SHOW SESSION STATUS LIKE 'Ssl_cipher_list'");
+            System.out.println("##### Session ssl status:");
             System.out.println();
-            System.out.println("##### Enabled cipher suites:");
+            rs = conn.createStatement().executeQuery("SHOW SESSION STATUS LIKE '%ssl%'");
             while (rs.next()){
-                System.out.println(rs.getString(2));
-            }
-
-            System.out.println();
-
-            rs = conn.createStatement().executeQuery("SHOW SESSION STATUS LIKE 'Ssl_cipher'");
-            System.out.println();
-            System.out.println("##### Cipher suite in use:");
-            while (rs.next()){
-                System.out.println(rs.getString(2));
+                System.out.println("##### " + rs.getString(1) + " : " + rs.getString(2));
             }
 
             System.out.println();
